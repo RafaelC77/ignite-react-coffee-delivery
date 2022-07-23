5514a66ed3e744e8df2b2caeaf392c50166b2235
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-07-23 16:59:43 -0300

    feat: add router file

---
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { LayouContainer } from "./styles";
+
+export function DefaultLayout() {
+  return (
+    <LayouContainer>
+      <Header />
+      <Outlet />
+    </LayouContainer>
+  );
+}
---
+++ src/layouts/DefaultLayout/styles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const LayouContainer = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+`;
