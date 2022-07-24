540c6c107ca5ab0921cd10430e2a1941c61542ee
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-07-24 19:00:41 -0300

    feat: create home component

--- src/layouts/DefaultLayout/styles.tsx
+++ src/layouts/DefaultLayout/styles.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const LayouContainer = styled.div`
-  max-width: 1120px;
+  max-width: 74rem;
   margin: 0 auto;
+  padding: 0 2rem;
 `;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { HomeComponent } from "./styles";
+
+export function Home() {
+  return (
+    <HomeComponent>
+      <h1>Home</h1>
+    </HomeComponent>
+  );
+}
