cf4ef850a78ef41d8fe5ad5fc21c2306dac3cd80
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-07-26 17:39:37 -0300

    feat: add selected coffees card to checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -3,23 +3,37 @@ import {
   CreditCard,
   CurrencyDollar,
   MapPinLine,
+  Minus,
   Money,
+  Plus,
+  Trash,
 } from "phosphor-react";
 import {
   AddressAndPaymentInfo,
   AddressForm,
   AditionalInfo,
   BaseInput,
+  ButtonContainer,
   CheckoutComponent,
+  CheckoutSummary,
   CityInfo,
+  CoffeeInfo,
+  ConfirmOrderButton,
+  DecrementButton,
+  IncrementButton,
   InputFields,
   NumberInput,
   PaymentMethod,
   PaymentOptions,
   PostalCodeInput,
+  Price,
+  RemoveButton,
+  SelectedCoffeesCard,
   SelectedCoffeesInfo,
   StateInfo,
   StreetInput,
+  TotalItensPrice,
+  TotalPrice,
 } from "./styles";
 
 export function Checkout() {
@@ -84,6 +98,95 @@ export function Checkout() {
 
         <SelectedCoffeesInfo>
           <h2>Cafés selecionados</h2>
+          <SelectedCoffeesCard>
+            <ul>
+              <li>
+                <img
+                  src="src/assets/coffee_images/american_espresso.png"
+                  alt=""
+                />
+                <CoffeeInfo>
+                  <span>Expresso Tradicional</span>
+                  <ButtonContainer>
+                    <div>
+                      <DecrementButton>
+                        <Minus />
+                      </DecrementButton>
+                      <input
+                        type="amount"
+                        id="price"
+                        step={1}
+                        placeholder="1"
+                        readOnly
+                      />
+                      <IncrementButton>
+                        <Plus />
+                      </IncrementButton>
+                    </div>
+
+                    <RemoveButton>
+                      <Trash />
+                      <span>REMOVER</span>
+                    </RemoveButton>
+                  </ButtonContainer>
+                </CoffeeInfo>
+                <Price>R$ 9,90</Price>
+              </li>
+
+              <li>
+                <img
+                  src="src/assets/coffee_images/american_espresso.png"
+                  alt=""
+                />
+                <CoffeeInfo>
+                  <span>Expresso Tradicional</span>
+                  <ButtonContainer>
+                    <div>
+                      <DecrementButton>
+                        <Minus />
+                      </DecrementButton>
+                      <input
+                        type="amount"
+                        id="price"
+                        step={1}
+                        placeholder="1"
+                        readOnly
+                      />
+                      <IncrementButton>
+                        <Plus />
+                      </IncrementButton>
+                    </div>
+
+                    <RemoveButton>
+                      <Trash />
+                      <span>REMOVER</span>
+                    </RemoveButton>
+                  </ButtonContainer>
+                </CoffeeInfo>
+                <Price>R$ 9,90</Price>
+              </li>
+            </ul>
+
+            <CheckoutSummary>
+              <div>
+                <span>Total de itens</span>
+                <TotalItensPrice>R$ 29,70</TotalItensPrice>
+              </div>
+              <div>
+                <span>Entrega</span>
+                <TotalItensPrice>R$ 3,50</TotalItensPrice>
+              </div>
+
+              <TotalPrice>
+                <span>Total</span>
+                <span>R$ 33,20</span>
+              </TotalPrice>
+            </CheckoutSummary>
+
+            <ConfirmOrderButton type="submit">
+              CONFIRMAR PEDIDO
+            </ConfirmOrderButton>
+          </SelectedCoffeesCard>
         </SelectedCoffeesInfo>
       </form>
     </CheckoutComponent>
